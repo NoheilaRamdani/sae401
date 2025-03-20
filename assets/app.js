4aa92fbcc7bfeba1f5b736f78a238acd5500143d
Flavie Harre <harreflavie@gmail.com>
2025-03-20 12:41:07 +0100

    Nav update

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,22 @@
 // assets/app.js
 console.log('AssetMapper test');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const activeItem = document.querySelector(".links li.active");
+
+    if (activeItem) {
+        // Cacher le separator APRÈS .active
+        const nextSeparator = activeItem.nextElementSibling;
+        if (nextSeparator && nextSeparator.classList.contains("separator")) {
+            nextSeparator.style.opacity = "0";
+            nextSeparator.style.visibility = "hidden";
+        }
+
+        // Cacher le separator AVANT .active
+        const prevSeparator = activeItem.previousElementSibling;
+        if (prevSeparator && prevSeparator.classList.contains("separator")) {
+            prevSeparator.style.opacity = "0";
+            prevSeparator.style.visibility = "hidden";
+        }
+    }
+});
