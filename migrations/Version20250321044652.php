be1e37ab16d6f60c9ab605cdbe28c7c96f3912b3
Flavie Harre <harreflavie@gmail.com>
2025-03-21 07:36:16 +0100

    Front - checked tasks

---
+++ migrations/Version20250321044652.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250321044652 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE suggestion ADD proposed_changes JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', CHANGE message message LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE suggestion DROP proposed_changes, CHANGE message message LONGTEXT NOT NULL');
+    }
+}
